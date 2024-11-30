3be0260e7529a995be83cdc1aad39c19557243fc
Arifur Rahman Sujon <arifurrahmansujon27@gmaile.com>
2024-11-30 22:22:55 +0600

    Refactored api call, now signup done, need some css fix

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,16 +1,17 @@
 import googleIcon from '../assets/google128.png';
-import { checkUserNameAvailability, signupSubmit } from '../services/api'
-import { useEffect, useState } from 'react';
+import { get, post } from '../services/api'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './css/Signup.css';
 
 export const Signup = () => {
-    const [userName, setUsername] = useState('');
-    const [name, setName] = useState('');
+    const [userName, setUserName] = useState('');
+    //const [name, setName] = useState('');
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
     const [cPassword, setCPassword] = useState('');
 
-    const [error, setError] = useState('');
+    const [userNameError, setUserNameError] = useState('');
     const [passError, setPassError] = useState('');
     const [cPassError, setCPassError] = useState('');
     const [mailError, setMailError] = useState('');
@@ -18,46 +19,60 @@ export const Signup = () => {
     const [responseMessage, setResponseMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
-    // Useename Validation function
-    const validateUsername = (value) => {
+    // Username Validation function
+    const validateUserName = (value) => {
         if (value.length < 3)
-            return 'Username must be at least 3 characters long.';
+            return 'UserName must be at least 3 characters long.';
 
         if (value.length > 20)
-            return 'Username must be at max 20 characters long.';
+            return 'UserName can be max 20 characters long.';
 
-        if (!/^[a-zA-Z0-9_.-]+$/.test(value))
-            return 'Username can only contain letters, numbers, dot, hyphen and underscores.';
+        if (!/^[a-zA-Z0-9._-]{3,20}$/.test(value))
+            return 'UserName can only contain letters, digits, underscores, hyphens, and dots';
 
         return '';
     };
 
     // Handle input change
-    const handleUsername = async (e) => {
-        const value = e.target.value;
-        setUsername(value);
+    const handleUserName = async (e) => {
+        const rawValue = e.target.value;
+        if (rawValue === null || rawValue === undefined || rawValue === '') {
+            setUserNameError('Username is required');
+            return;
+        }
 
-        const validationError = validateUsername(value);
-        setError(validationError);
+        const userName = rawValue.trim();
+        setUserName(userName);
 
-        if (validationError == '') {
-            const v = await checkUserNameAvailability(value);
-            v == false ? setUsername(value) : setError('Username has been taken!');
+        const validationError = validateUserName(userName);
+        setUserNameError(validationError);
+
+        if (validationError === '') {
+            const paramMap = { userName: userName };
+            const { status, data } = await get('/signup/checkUserName', paramMap);
+            if (status === 200 && data) {
+                setUserNameError('UserName has been taken!');
+            }
         }
     };
     
-    const isValidEmail = (email) => {
+    const isEmailValid = (email) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
     
     const handleMail = (e) => {
-        const value = e.target.value;
-        setMail(value);
+        const rawValue = e.target.value;
+        if (rawValue === null || rawValue === undefined || rawValue === '') {
+            setMailError('Email is required');
+            return;
+        }
 
-        if (mail == null) setMailError('Email is required');
-        if (isValidEmail(value) == false) setMailError('Invalid Mail.')
+        const email = rawValue.trim();
+        setMail(email);
 
+        if (!isEmailValid(email)) 
+            setMailError('Invalid Mail.')
     }
     
     const validatePassword = (password) => {
@@ -65,25 +80,41 @@ export const Signup = () => {
         if (!passwordRegex.test(password))
             return 'Password must be 6-64 characters long, contain at least one uppercase letter, one lowercase letter, one digit, and one special character.';
 
+        return '';
     }
     
     const handlePassword = (e) => {
-        const value = e.target.value;
-        setPassword(value);
+        const rawValue = e.target.value;
+        if (rawValue === null || rawValue === undefined || rawValue === '')
+            return;
 
-        const validationError = validatePassword(value);
+        const password = rawValue.trim();
+        setPassword(password);
+
+        const validationError = validatePassword(password);
         setPassError(validationError);
     }
 
     //handle Confirm Password
     const handleCPassword = (e) => {
-        const value = e.target.value;
-        setCPassword(value);
-        if (password !== value) setCPassError('Passwords don not match.');
+        const rawValue = e.target.value;
+        if (rawValue === null || rawValue === undefined || rawValue === '')
+            return;
+
+        const cPassword = rawValue.trim();
+        setCPassword(cPassword);
+        if (password !== cPassword) setCPassError('Passwords do not match.');
+    }
+
+    const anyError = () => {
+        return userNameError !== '' || mailError !== '' || passError !== '' || cPassError !== '';
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setErrorMessage('');
+
+        if (anyError()) return;
 
         const signupRequest = {
             "userName": userName,
@@ -92,19 +123,22 @@ export const Signup = () => {
             "reTypeRawPassword": cPassword
         }
 
-        try {
-            const response = await signupSubmit(signupRequest);
-            console.log('response: ', response);
+        const { status, data } = await post('/signup', signupRequest);
+        if (status >= 200 && status < 300) {
             setResponseMessage("Signup successful!"); // Success message
-            setErrorMessage(""); // Clear any error
-        } catch (error) {
-            console.error('error status: ', error.response?.status);
-            if (error.response?.data) {
-                console.error('error data: ', error.response?.data);
+        } else {
+            if (data === null || data === undefined) {
+                setErrorMessage("An error occurred");
+                return;
             }
-            setResponseMessage("");
-            setErrorMessage(error.response?.data?.message || "An error occurred");
-        }
+
+            if ('userName' in data)
+                setUserNameError(data.userName);
+            if ('email' in data)
+                setMailError(data.email);
+            if ('signupRequest' in data)
+                setCPassError(data.signupRequest);
+        }  
 
     }
 
@@ -115,11 +149,11 @@ export const Signup = () => {
                 <input
                     type="text"
                     placeholder="username"
-                    onBlur={handleUsername}
-                    onFocus={() => setError('')}
+                    onBlur={handleUserName}
+                    onFocus={() => setUserNameError('')}
                     required
                 />
-                {error && <p style={{ color: 'red' }}>{error}</p>}
+                {userNameError && <p className="error-style">{userNameError}</p>}
                 <input
                     type="text"
                     placeholder="Your Name"
@@ -132,7 +166,7 @@ export const Signup = () => {
                     onFocus={() => { setMailError('') }}
                     required
                 />
-                {mailError && <p style={{ color: 'red' }}>{mailError}</p>}
+                {mailError && <p className="error-style">{mailError}</p>}
                 <input
                     type="password"
                     placeholder="password*"
@@ -140,7 +174,7 @@ export const Signup = () => {
                     onFocus={() => { setPassError('') }}
                     required
                 />
-                {passError && <p style={{ color: 'red' }}>{passError}</p>}
+                {passError && <p className="error-style">{passError}</p>}
                 <input
                     type="password"
                     placeholder="Confirm Password*"
@@ -148,12 +182,12 @@ export const Signup = () => {
                     onFocus={()=>setCPassError('')}
                     required
                 />
-                {cPassError && <p style={{ color: 'red' }}>{cPassError}</p>}
+                {cPassError && <p className="error-style">{cPassError}</p>}
                 <button type='submit'>
                     Sign Up
                 </button>
-                {responseMessage && <p style={{ color: "green" }}>{responseMessage}</p>}
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+                {responseMessage && <p className="success-style">{responseMessage}</p>}
+                {errorMessage && <p className="error-style" style={{ marginTop: '0px' }}>{errorMessage}</p>}
                 {/* <input type="button" id="signup" value="Sign Up"  /> */}
             </form>
             <div style={{ paddingTop: "25px" }}>Already have an account? <Link to="/login" style={{ color: "green" }}>Log In</Link></div>
--- src/services/api.js
+++ src/services/api.js
@@ -4,18 +4,94 @@ const api = axios.create({
     baseURL: "http://localhost:8080/api/v1",
 });
 
-export const getPosts = () =>{
-    return api.get("/posts");
+const getUrl = (path, paramMap) => {
+    let url = path;
+    if (paramMap) {
+        url += '?' + Object.entries(paramMap).map(([key, value]) => `${key}=${value}`).join('&');
+    }
+    return url;
+}
+
+const handleError = error => {
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+
+        console.error('Error status: ', error.response.status);
+        //console.error('Error headers: ', error.response.headers);
+        if (error.response.data) {
+            console.error('Error data: ', error.response.data);
+            return {
+                status: error.response.status,
+                data: error.response.data
+            }
+        } else {
+            console.warn('No data in error response');
+            return {
+                status: error.response.status
+            }
+        }
+
+    } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.error('Error request: ', error.request);
+        return {
+            data: error.request
+        }
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error message: ', error.message);
+        return {
+            data: error.message
+        }
+    }
+}
+
+export const get = async (path, paramMap) => {
+    try {
+        const url = getUrl(path, paramMap);
+        console.info('get URL: ', url);
+
+        const response = await api.get(url);
+        console.log('Status: ', response.status);
+
+        if (response.data !== null && response.data !== undefined) {
+            console.info('response data: ', response.data);
+            return { status: response.status, data: response.data };
+        } else {
+            console.warn('No data in response');
+            return { status: response.status };
+        }
+    } catch (error) {
+        return handleError(error);
+    }
+}
+
+export const post = async (path, body) => {
+    try {
+        const url = getUrl(path);
+        console.info('post URL: ', url);
+        const response = await api.post(url, body);
+        console.log('Status: ', response.status);
+
+        if (response.data !== null && response.data !== undefined) {
+            console.info('response data: ', response.data);
+            return { status: response.status, data: response.data };
+        } else {
+            console.warn('No data in response');
+            return { status: response.status };
+        }
+    } catch (error) {
+        return handleError(error);
+    }
+}
+
+export const getPosts = () => {
+    return post('/posts');
 }
 
 export const checkUserNameAvailability = async (value) => {
-    const response = await api.get(`/signup/checkUserName?userName=${value}`);
-    console.log('Status: ', response.status);
-    return response.data;
-};
-
-export const signupSubmit = async(formData) => {
-    const response = await api.post(`/signup`, formData);
-    console.log('Status: ', response.status);
-    return response.data;
+    return get('/signup/checkUserName', { userName: value });
 };
