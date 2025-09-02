7414ed0ff9ed0cc2328b0046c9e5d786aa240a1f
Arifur Rahman Sujon <arifurrahmansujon27@gmaile.com>
2025-09-02 15:49:06 +0600

    after login user will be redirected to incident page

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -4,17 +4,24 @@ import PropTypes from 'prop-types';
 
 import { get, post, auth } from '../services/api';
 import UserContext from "./UserContext";
+import { useAppNavigate } from '../hooks/useAppNavigate';
 
 
 export const UserProvider = ({ children }) => {
+    const { goLogin, goIncidents } = useAppNavigate();
     const [user, setUser] = useState(null); // { id, name, email, role }
 
-    const fetchUserInfo = async () => {
+    const fetchUserInfo = async (callback) => {
         const { status, data } = await get(auth, "/users/me", { withCredentials: true });
         if (status === 200 && data) {
             setUser(data)
             //localStorage.setItem("userInfo", JSON.stringify(data));
             //setIsLoggedIn(true);
+            
+            // if (typeof callback === "function") {
+            //     callback();
+            // }
+            callback?.();
         } else {
             logout();
         }
@@ -26,19 +33,22 @@ export const UserProvider = ({ children }) => {
     }, []);
 
     const login = () => {
-        fetchUserInfo();
+        fetchUserInfo(() => goIncidents());
         //localStorage.setItem("user", JSON.stringify(userData));
     };
 
-    const logoutFromServer = async () => {
+    const logoutFromServer = async (callback) => {
         const { status } = await post(auth, "/logout");
         if (status === 200) {
-            setUser(null);
+            callback();
         }
     }
 
     const logout = () => {
-        logoutFromServer();
+        logoutFromServer( () => {
+            setUser(null);
+            goLogin();
+        });
         //localStorage.removeItem("user");
     };
 
