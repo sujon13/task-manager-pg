79ffb27c8d1dc663b408d4f90414f8267822bf45
Arifur Rahman Sujon <arifurrahmansujon27@gmaile.com>
2025-09-07 10:19:29 +0600

    loggedin users will be redirected to incients page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,9 @@
-import { get, qa } from '../services/api';
 import useUser from '../hooks/useUser';
+import { useAppNavigate } from '../hooks/useAppNavigate';
 
 export const Home = () => {
     const { isLoggedIn } = useUser();
-
-    
-    const test = async () => {
-        try {
-            const response = await get(qa, '/posts', { withCredentials: true });
-            console.log('status: ' + response.status);
-            console.log('posts: ' + response.data);
-        } catch (error) {
-            console.error('Error fetching posts:', error);
-        }
-    }
+    const { goIncidents } = useAppNavigate();
 
     if (isLoggedIn) {
         //test();
@@ -22,12 +12,10 @@ export const Home = () => {
     return (
         <div className='d-flex justify-content-center align-items-center' style={{ marginTop: '20px' }}>
             {isLoggedIn ? (
-                <h2>Welcome to Power Grid Task Tracker</h2>
+                goIncidents()
             ) : (
                 <h2>Please login to continue to Power Grid Task Tracker</h2>
             )}
-            {/* <div><Link to="/signup">Sign Up</Link></div>
-            <div><Link to="/login">Log In</Link></div> */}
         </div>
     )
 }
