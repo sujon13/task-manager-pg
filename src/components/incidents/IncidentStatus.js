45ca074d82b0ec5d9dc5c5fe9c7477a3cae9377a
Arifur Rahman Sujon <arifurrahmansujon27@gmaile.com>
2025-09-23 12:15:25 +0600

    rempved in_review from incidentstatus

--- src/components/incidents/IncidentStatus.js
+++ src/components/incidents/IncidentStatus.js
@@ -3,6 +3,5 @@ export const IncidentStatus = Object.freeze({
     IN_PROGRESS:   Object.freeze({ key: "IN_PROGRESS", value: 1, displayName: "In Progress" }),
     COMPLETED:     Object.freeze({ key: "COMPLETED", value: 2, displayName: "Completed" }),
     RETURNED:      Object.freeze({ key: "RETURNED", value: 3, displayName: "Returned" }),
-    IN_REVIEW:     Object.freeze({ key: "IN_REVIEW", value: 4, displayName: "Under Observation" }),
-    RESOLVED:      Object.freeze({ key: "RESOLVED", value: 5, displayName: "Resolved" })
+    RESOLVED:      Object.freeze({ key: "RESOLVED", value: 4, displayName: "Resolved" })
 });
